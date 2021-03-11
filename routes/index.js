0e13a92127f7f0fc9e99fb27e272dc0b317973a1
Chaokai Lai <chaokai.lai@gmail.com>
2021-03-11 20:53:19 +1000

    add eslint rule max-len(max 80) add route /users - get all users

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,14 @@ const express = require('express')
 const hotel = require('./hotel.router')
 const rooms = require('./rooms.router')
 const reservations = require('./reservations.router')
+const user = require('./user')
 
 const router = express.Router()
 
 router.use('/hotel', hotel)
 router.use('/rooms', rooms)
 router.use('/reservations', reservations)
+router.use('/user', user)
 
 module.exports = router
 
