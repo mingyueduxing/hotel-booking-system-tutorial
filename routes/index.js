5b31cc406c04b1543829ee69be30866e6887ad08
XUE_wallace <xue.huang@uqconnect.edu.au>
2021-03-11 20:44:28 +1000

    commit router user

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,15 @@
 const express = require('express')
 const hotel = require('./hotel.router')
 const rooms = require('./rooms.router')
-const reservations = require('./reservations.router')
+const reservations = require('./reservations.router'),
+const user = require("./user.router")
 
 const router = express.Router()
 
 router.use('/hotel', hotel)
 router.use('/rooms', rooms)
 router.use('/reservations', reservations)
+router.use('/user', user)
 
 module.exports = router
 
