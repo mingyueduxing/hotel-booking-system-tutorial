b44625f377158c72bcea9b301433ff49d81ac337
Jackie Tian <>
2021-03-14 20:06:39 +1100

    add sequelize

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
+require('dotenv').config()
 const express = require('express')
 const routes = require('./routes')
+const { sequelize } = require('./models')
 
 const app = express()
 
 app.use('/api', routes);
+
+sequelize.sync()
 
 app.listen('3000', () => {
 	console.log('App started at PORT 3000')
